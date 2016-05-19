2c62df6cb8a11de42c300f0a6b4a4d1cd1e8bae6
Skyler Braden <skylerbraden@me.com>
2016-05-19 16:39:59 -0500

    got IG API to return object with data

--- app/scripts/controllers/JourneyMapCtrl.js
+++ app/scripts/controllers/JourneyMapCtrl.js
@@ -1,6 +1,26 @@
 (function() {
     function JourneyMapCtrl() {
-        
+		
+		vm = this;
+
+		vm.instaApi = function(data){
+			console.log(1)
+			console.log(data);
+		};
+
+		$.ajax({
+			url: "https://api.instagram.com/v1/locations/399440619/media/recent?access_token=5405469.0f878fa.ccfcfe7552a7470abb6aebf116bfc7b6",
+			type: 'get',
+			dataType: 'jsonp',
+			crossOrigin: true,
+			jsonpCallback: "vm.instaApi",
+			cache: true
+		});
+		
+		$("#test-list").append("<li>Testing controller</li>");
+
+		
+		
 	}
     angular
         .module('journeygram')
