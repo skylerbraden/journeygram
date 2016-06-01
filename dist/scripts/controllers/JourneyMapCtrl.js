a07416f4749ff0ae4e4e46e58b4094983a51cbcf
Skyler Braden <skylerbraden@me.com>
2016-06-01 11:42:42 -0500

    Trying to create a function to add markers to map

--- dist/scripts/controllers/JourneyMapCtrl.js
+++ dist/scripts/controllers/JourneyMapCtrl.js
@@ -1,19 +1,19 @@
 (function() {
     function JourneyMapCtrl($scope) {
-		
+
 		vm = this;
 		vm.latList = [];
-		
+
 		vm.instaApi = function(photo){
 //			console.log("this is just a test")
 //			console.log(photo.data);
 			var latitude = photo.data[0].location.latitude;
-			
 
-			
+
+
 //			console.log("Inside instaApi function");
 //			console.log(vm.latList);
-			
+
 			$scope.$apply(function(){
 				for (var i=0; i < photo.data.length; i++) {
 					vm.latList.push(photo.data[i].location.latitude);
@@ -32,22 +32,22 @@
 			jsonpCallback: "vm.instaApi",
 			cache: true
 		});
-		
-	
+
+
 		$("#test-list").append("<li>Test</li>")
-		
+
 		window.foo = vm.instaApi;
-		
-		//I want these console.logs to fire AFTER the vm.instaApi function has run so that teh array vm.latList is accessible to me. How do I do this?
+
+		//I want these console.logs to fire AFTER the vm.instaApi function has run so that the array vm.latList is accessible to me. How do I do this?
 //		console.log("Outside instaApi function")
 //		console.log("1: "+ vm.latList);
-//		
-		
+//
 
 
 
-	
-		
+
+
+
 	}
     angular
         .module('journeygram')
