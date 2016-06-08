5677194a57f9f14fb5e5883ab98e02a07fb4b85f
Skyler Braden <skylerbraden@me.com>
2016-06-08 12:01:43 -0500

    added Google map to cotroller

--- app/scripts/controllers/JourneyViewCtrl.js
+++ app/scripts/controllers/JourneyViewCtrl.js
@@ -1,8 +1,29 @@
 (function() {
-    function JourneyViewCtrl(Cities) {
+    function JourneyViewCtrl() {
 
-        console.log("The controller is working.");
-        console.log(Cities[1]);
+        var littleton = {lat: 39.6133, lng: -105.0166};
+        var coloSprings = {lat: 38.8339, lng: -104.8214};
+
+        (function initMap() {
+            var map = new google.maps.Map(document.getElementById('journey-map'), {
+                center: littleton,
+                scrollwheel: false,
+                zoom: 8
+            });
+
+            var addMarker = function(newLat, newLng, markerTitle) {
+                var newMarker = new google.maps.Marker({
+                    position: {lat: newLat, lng: newLng},
+                    map: map,
+                    title: markerTitle
+                });
+            }
+
+            // addMarker(coloSprings.lat, coloSprings.lng, "Co. Springs");
+            // addMarker(littleton.lat, littleton.lng, "Littleton");
+
+        })();
+
 
 
 
@@ -10,5 +31,5 @@
 
     angular
         .module('journeygram')
-        .controller('JourneyViewCtrl', ['Cities', JourneyViewCtrl]);
+        .controller('JourneyViewCtrl', [JourneyViewCtrl]);
 })();
