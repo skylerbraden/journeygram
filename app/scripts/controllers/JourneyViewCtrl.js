90491c18dddbe5fd25f8d0b55a6fbc8946c2886a
Skyler Braden <skylerbraden@me.com>
2016-06-22 18:01:58 -0500

    Added markers for photos and city titles

--- app/scripts/controllers/JourneyViewCtrl.js
+++ app/scripts/controllers/JourneyViewCtrl.js
@@ -4,11 +4,12 @@
         var littleton = {lat: 39.6133, lng: -105.0166};
         var coloSprings = {lat: 38.8339, lng: -104.8214};
         var london = {lat: 51.5074, lng: 0.1278};
+        var nyc = {lat: 40.7128, lng: -74.0059};
 
         vm.map = new google.maps.Map(document.getElementById('journey-map'), {
-            center: littleton,
+            center: nyc,
             scrollwheel: false,
-            zoom: 8
+            zoom: 3
         });
 
         // vm.addMarker = function() {
@@ -26,27 +27,47 @@
 
 
         //Instagram
-        vm.latList = [];
         vm.photoList = [];
         vm.locationList = [];
 
         instaApi = function(photo){
             console.log(photo.data);
-            // var latitude = photo.data[0].location.latitude;
-
-            // console.log(vm.latList);
 
             $scope.$apply(function(){
                 for (var i=0; i < photo.data.length; i++) {
-                    vm.latList.push(photo.data[i].location.latitude);
-                    vm.photoList.push(photo.data[i].images);
-                    vm.locationList.push(photo.data[i].location);
-                }
+                    vm.photoList.push({time: photo.data[i].created_time, image: photo.data[i].images.thumbnail, name: photo.data[i].location.name});
+                    vm.locationList.push({time: photo.data[i].created_time, lat: photo.data[i].location.latitude, lng: photo.data[i].location.longitude});
 
+                    new google.maps.Marker({
+                        map: vm.map,
+                        position: {lat: photo.data[i].location.latitude, lng: photo.data[i].location.longitude},
+                        // title: 'Title'
+                    });
+                }
+            });
+
+            // Directions between Instagrams
+            var directionsDisplay = new google.maps.DirectionsRenderer({
+              map: vm.map
+            });
+
+            // Set destination, origin and travel mode.
+            var request = {
+              destination: indianapolis,
+              origin: chicago,
+              travelMode: google.maps.TravelMode.DRIVING
+            };
+
+            // Pass the directions request to the directions service.
+            var directionsService = new google.maps.DirectionsService();
+            directionsService.route(request, function(response, status) {
+              if (status == google.maps.DirectionsStatus.OK) {
+                // Display the route on the map.
+                directionsDisplay.setDirections(response);
+              }
             });
 
         };
-
 
         $.ajax({
             url: "https://api.instagram.com/v1/tags/journeygram/media/recent?access_token=3264274466.d8576bc.77adee700b254e958a8bbb6967a61142",
@@ -58,12 +79,20 @@
         });
 
 
+
         // Plot Instagram locations on Google Map
-        // var instaMarker = new google.maps.Marker({
-        //     map: vm.map,
-        //     position: {vm.locationList[0].latitude, vm.locationList[0].longitude},
-        //     title: 'London!'
-        // });
+        // var instaMarker = function() {
+        //   for (var i=0; 1 < vm.locationList.length; i++) {
+        //     new google.maps.Marker({
+        //         map: vm.map,
+        //         position: {lat: vm.locationList[i].latitude, lng: vm.locationList[i].longitude},
+        //         // title: 'Title'
+        //     });
+        //   }
+        //   console.log(vm.locationList);
+        // };
+
+
 
 
     }
