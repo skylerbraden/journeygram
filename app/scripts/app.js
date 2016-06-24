5fec2d16948616b8e0fc1f8462ab8a3cf643c170
Skyler Braden <skylerbraden@me.com>
2016-06-24 10:15:48 -0500

    added some styling to JourneyGram

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,63 +6,60 @@
                 requireBase: false
         });
 
-
-
-      // $urlRouterProvider
-      //   .otherwise('/');
-
         $stateProvider
             .state('instagram', {
                 url: '/instagram',
                 controller: 'InstagramCtrl as instagram',
                 templateUrl: '/templates/instagram.html'
             })
+
 			.state('journeyview', {
 				url: '/',
 				controller: 'JourneyViewCtrl as journeyView',
 				templateUrl: '/templates/journeyview.html',
-        resolve: {
-          "hasInstagram" : function($location){
-            if($location.$$search.code != undefined){
-              console.log("trying to json P")
-              $.ajax({
-                  url: "https://api.instagram.com/v1/tags/journeygram/media/recent?access_token=" + $location.$$search.code,
-                  type: 'get',
-                  dataType: 'jsonp',
-                  crossOrigin: true,
-                  jsonpCallback: "instaApi",
-                  cache: true
-              });
-              return "?"
-            }else{
-                return "no access_token"
-            }
-          }
-        }
+                resolve: {
+                  "hasInstagram" : function($location){
+                    if($location.$$search.code != undefined){
+                      console.log("trying to json P")
+                      $.ajax({
+                          url: "https://api.instagram.com/v1/tags/journeygram/media/recent?access_token=" + $location.$$search.code,
+                          type: 'get',
+                          dataType: 'jsonp',
+                          crossOrigin: true,
+                          jsonpCallback: "instaApi",
+                          cache: true
+                      });
+                      return "?"
+                    }else{
+                        return "no access_token"
+                    }
+                  }
+                }
 			})
-      .state('instaAuth', {
-        url: '/access_token={accessToken}',
-        controller: 'JourneyViewCtrl as journeyView',
-        templateUrl: '/templates/journeyview.html',
-        resolve: {
-          "hasInstagram" : function($location, $stateParams){
-            if($stateParams.accessToken != undefined){
-              console.log("trying to json P")
-              $.ajax({
-                  url: "https://api.instagram.com/v1/tags/journeygram/media/recent?access_token=" + $stateParams.accessToken,
-                  type: 'get',
-                  dataType: 'jsonp',
-                  crossOrigin: true,
-                  jsonpCallback: "instaApi",
-                  cache: true
-              });
-              return "?"
-            }else{
-                return "no access_token"
+
+          .state('instaAuth', {
+            url: '/access_token={accessToken}',
+            controller: 'JourneyViewCtrl as journeyView',
+            templateUrl: '/templates/journeyview.html',
+            resolve: {
+              "hasInstagram" : function($location, $stateParams){
+                if($stateParams.accessToken != undefined){
+                  console.log("trying to json P")
+                  $.ajax({
+                      url: "https://api.instagram.com/v1/tags/journeygram/media/recent?access_token=" + $stateParams.accessToken,
+                      type: 'get',
+                      dataType: 'jsonp',
+                      crossOrigin: true,
+                      jsonpCallback: "instaApi",
+                      cache: true
+                  });
+                  return "?"
+                }else{
+                    return "no access_token"
+                }
+              }
             }
-          }
-        }
-      })
+          })
     }
 
     angular
